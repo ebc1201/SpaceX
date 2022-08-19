24e80504f8ea3898b36ee102ee001167955cb186
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2022-08-18 19:26:13 -0500

    added final pages and javascript for counter

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu')
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage)
 
 function navToggle() {
     btn.classList.toggle('open');
@@ -10,3 +13,44 @@ function navToggle() {
     document.body.classList.toggle('stop-scrolling');
     menu.classList.toggle('show-menu')
 }
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  };
+}
+
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+        // Get count target
+        const updateCounter = () => {
+          const target =  +counter.getAttribute('data-target');
+          // Get current counter value
+          const c = +counter.innerText;
+          // Create an Increment
+          const increment = target / 100;
+          // If counter is less than the target, add increment
+          if (c < target) {
+            // round up and set counter value
+            counter.innerText = `${Math.ceil(c + increment)}`
+            
+            setTimeout(updateCounter, 75);
+        } else {
+          counter.innerText =  target;
+        }
+      };
+        
+      updateCounter();
+    });
+};
+
+function reset() {
+  counters.forEach((counter) => counter.innerHTML = '0');
+};
